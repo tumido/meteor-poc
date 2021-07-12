1220b8ef2555aa835abe9119978631b3f00c83f0
Tomas Coufal <tcoufal@redhat.com>
2021-07-12 14:09:13 +0200

    feat: Simplify PoC preview into a scaffold
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- backend/pkg/api/order.go
+++ backend/pkg/api/order.go
@@ -2,33 +2,57 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/operate-first/meteor/pkg/store"
+	"github.com/patrickmn/go-cache"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"k8s.io/klog/v2"
 )
 
 type Order struct {
-	Url  string
-	Uuid types.UID
+	Url     string    `json:"url"`
+	Uuid    types.UID `json:"uuid"`
+	Created time.Time `json:"created"`
+	Status  []string  `json:"status"`
+}
+
+var globalStore = cache.New(24*time.Hour, 30*time.Minute)
+
+func Set(order *Order) {
+	globalStore.Set(string(order.Uuid), order, 0)
+}
+
+func Get(key string) (*Order, error) {
+	val, found := globalStore.Get(key)
+	if !found {
+		return nil, errors.New("Order not found")
+	}
+
+	return val.(*Order), nil
 }
 
 func OrderEndpoint(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		var order Order
+		order := Order{
+			Uuid:    uuid.NewUUID(),
+			Created: time.Now(),
+			Status:  []string{"Order received"},
+		}
 		err := json.NewDecoder(r.Body).Decode(&order)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		order.Uuid = uuid.NewUUID()
 		klog.Infof("Storing order %s", order.Uuid)
-		store.Set(fmt.Sprintf("%s", order.Uuid), order.Url)
+
+		Set(&order)
+		go processOrder(&order)
 
 		w.Header().Set("Location", fmt.Sprintf("/order/%s", order.Uuid))
 		w.WriteHeader(http.StatusCreated)
@@ -41,16 +65,40 @@ func OrderEndpoint(w http.ResponseWriter, r *http.Request) {
 		}
 
 		klog.Infof("Fetching order %s", uuid[0])
-		url := store.Get(uuid[0])
-		if url == "" {
+		order, err := Get(uuid[0])
+		if err != nil {
+			klog.Errorf("Order get failed: %s", err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		if order.Uuid == "" {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"uuid": uuid[0], "url": url})
+		data, err := json.Marshal(order)
+		if err != nil {
+			klog.Errorf("Order get failed: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
 
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+}
+
+func processOrder(order *Order) {
+	go triggerJupyterBookPipeline(order)
+	go triggerImageBuildPipeline(order)
+}
+
+func triggerImageBuildPipeline(order *Order) {
+	order.Status = append(order.Status, "Image build started")
+}
+
+func triggerJupyterBookPipeline(order *Order) {
+	order.Status = append(order.Status, "JupyterBook build started")
 }
